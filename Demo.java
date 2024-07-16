7c0c3f061eb2c3266f4b5747d84913ea82533718
RAHUL S H <85486874+Rahulsh006@users.noreply.github.com>
2024-07-16 10:57:37 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -3,58 +3,58 @@ Copyright (C) Deepali Srivastava - All Rights Reserved
 This code is part of DSA course available on CourseGalaxy.com    
 */
 
-package circularQueue;
+package deque;
 
 import java.util.Scanner;
 
-public class Demo
+public class Demo 
 {
 	public static void main(String[] args) 
 	{
-	      int choice,x;
-	      Scanner scan = new Scanner(System.in);	
-	
-	      CircularQueue qu = new CircularQueue(8);
+		int choice,x;
 		
-	      while(true)
-	      {
-		     System.out.println("1.Insert an element in the queue");
-		     System.out.println("2.Delete an element from the queue");
-		     System.out.println("3.Display element at the front");
-		     System.out.println("4.Display all elements of the queue");
-		     System.out.println("5.Display size of the queue");
-	      	 System.out.println("6.Quit");
-		     System.out.print("Enter your choice : ");
-		     choice = scan.nextInt();
-		     if(choice==6)
-			   break;
-	
-		    switch(choice)
-		    {
-		      case 1:
-			    System.out.print("Enter the element to be inserted : ");
-			    x = scan.nextInt();
-			    qu.insert(x);
-			    break;
-		      case 2:
-			    x=qu.Delete();
-			    System.out.println("Element deleted is : " + x);
-			    break;
-		      case 3:
-			    System.out.println("Element at the front is : " + qu.peek());
-			    break;
-		      case 4:
-			    qu.display();
-			    break;
-		      case 5:
-			    System.out.println("Size of queue is " + qu.size());
-			    break;
-		      default:
-			    System.out.println("Wrong choice");
-		  }
-		System.out.println();
-		
+		Scanner scan = new Scanner(System.in);	
+		DequeA dq = new DequeA(8);
+
+		while(true)
+		{
+			System.out.println("1.Insert at the front end");
+			System.out.println("2.Insert at the rear end");
+			System.out.println("3.Delete from front end");
+			System.out.println("4.Delete from rear end");
+			System.out.println("5.Display all elements of deque");
+			System.out.println("6.Quit");
+			System.out.print("Enter your choice : ");
+			choice = scan.nextInt();
+			
+			if(choice==6)
+				break;
+
+			switch(choice)
+			{
+			case 1:
+				System.out.print("Enter the element to be inserted : ");
+				x = scan.nextInt();
+				dq.insertFront(x);
+				break;
+			case 2:
+				System.out.print("Enter the element to be inserted : ");
+				x = scan.nextInt();
+				dq.insertRear(x);
+				break;
+			 case 3:
+				System.out.println("Element deleted from front end is " + dq.deleteFront());
+				break;
+			 case 4:
+				System.out.println("Element deleted from rear end is  " + dq.deleteRear());
+				break;
+			 case 5:
+				dq.display();
+				break;
+			 default:
+				System.out.println("Wrong choice");
+			}
+		}
+		scan.close();
 	}
-	 scan.close();
-  }
 }
