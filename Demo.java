8a57cfbc8ce4352d82cde4e18184d5c00fd86aea
RAHUL S H <85486874+Rahulsh006@users.noreply.github.com>
2024-07-16 11:00:38 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -3,57 +3,83 @@ Copyright (C) Deepali Srivastava - All Rights Reserved
 This code is part of DSA course available on CourseGalaxy.com    
 */
 
-package queueLinked;
-
+package circularLinkedList;
 import java.util.Scanner;
 
-public class Demo
+public class Demo 
 {
 	public static void main(String[] args) 
 	{
-	      int choice,x;
-	      Scanner scan = new Scanner(System.in);	
-	
-	      QueueL qu = new QueueL();
+		int choice,data,x;
+		Scanner scan = new Scanner(System.in); 
+		CircularLinkedList List = new CircularLinkedList();
 		
-	      while(true)
-	      {
-		     System.out.println("1.Insert an element in the queue");
-		     System.out.println("2.Delete an element from the queue");
-		     System.out.println("3.Display element at the front");
-		     System.out.println("4.Display all elements of the queue");
-		     System.out.println("5.Display size of the queue");
-	      	 System.out.println("6.Quit");
-		     System.out.print("Enter your choice : ");
-		     choice = scan.nextInt();
-		     if(choice==6)
-			   break;
-	
-		    switch(choice)
-		    {
-		      case 1:
-			    System.out.print("Enter the element to be inserted : ");
-			    x = scan.nextInt();
-			    qu.insert(x);
-			    break;
-		      case 2:
-			    x=qu.Delete();
-			    System.out.println("Element deleted is : " + x);
-			    break;
-		      case 3:
-			    System.out.println("Element at the front is : " + qu.peek());
-			    break;
-		      case 4:
-			    qu.display();
-			    break;
-		      case 5:
-			    System.out.println("Size of queue is " + qu.size());
-			    break;
-		      default:
-			    System.out.println("Wrong choice");
-		  }
-		System.out.println();
-	}
-	scan.close();
-  }
+		List.createList();
+			
+		while(true)
+		{
+			System.out.println("1.Display List");
+			System.out.println("2.Insert in  empty list");
+			System.out.println("3.Insert in the beginning");
+			System.out.println("4.Insert at the end");
+			System.out.println("5.Insert after a node");
+			System.out.println("6.Delete first node");
+			System.out.println("7.Delete last node");
+			System.out.println("8.Delete any node");
+			System.out.println("9.Quit");
+			
+			System.out.print("Enter your choice : ");
+			choice = scan.nextInt();
+
+			if(choice==9)
+				break;
+			
+			switch(choice)
+			{
+			 case 1:
+				List.displayList();
+				break;
+			 case 2:
+				System.out.print("Enter the element to be inserted : ");
+				data = scan.nextInt();
+				List.insertInEmptyList(data);
+				break;
+			 case 3:
+				System.out.print("Enter the element to be inserted : ");
+				data = scan.nextInt();
+				List.insertInBeginning(data);
+				break;
+			 case 4:
+				System.out.print("Enter the element to be inserted : ");
+				data = scan.nextInt();
+				List.insertAtEnd(data);
+				break;
+			 case 5:
+				System.out.print("Enter the element to be inserted : ");
+				data = scan.nextInt();
+				System.out.print("Enter the element after which to insert : ");
+				x = scan.nextInt();
+				List.insertAfter(data,x);
+				break;
+			 case 6:
+				 List.deleteFirstNode();
+				 break;
+			 case 7:
+				 List.deleteLastNode();
+				 break;
+			 case 8:
+				System.out.print("Enter the element to be deleted : ");
+				data = scan.nextInt();
+				List.deleteNode(data);
+				break;
+			 default:
+				System.out.println("Wrong choice");
+			}/*End of switch*/
+			System.out.println();	
+		}/*End of while*/
+		scan.close();
+		System.out.println("Exiting");
+	}/*End of main( )*/
 }
+
+
