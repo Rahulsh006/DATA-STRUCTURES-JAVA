382431782525cb7c5e41a76608fb1ac9d6ec8fcc
RAHUL S H <85486874+Rahulsh006@users.noreply.github.com>
2024-07-18 10:27:52 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -3,74 +3,77 @@ Copyright (C) Deepali Srivastava - All Rights Reserved
 This code is part of DSA course available on CourseGalaxy.com    
 */
 
-package HeaderList;
-
+package parentheses;
 import java.util.Scanner;
 
 public class Demo 
 {
 	public static void main(String[] args) 
 	{
-		int choice,data,x,k;
-		Scanner scan = new Scanner(System.in); 
+	  String expression;
+
+	  Scanner scan = new Scanner(System.in);	
+	  
+	  System.out.print("Enter an expression with parentheses : ");
+	  expression = scan.nextLine();
 		
-		HeaderList List = new HeaderList();
-		List.createList();
-		
-		while(true)
-		{
-			System.out.println("1.Display list");
-			System.out.println("2.Insert a node at the end of the list");
-			System.out.println("3.Insert a new node before a node");
-			System.out.println("4.Insert at a given position");
-			System.out.println("5.Delete a node");
-			System.out.println("6.Reverse the list");
-			System.out.println("7.Quit");
-			System.out.println("Enter your choice : ");
-			choice=scan.nextInt();
-			
-			if(choice==7)
-				break;
+	  if(isValid(expression))
+		  System.out.println("Valid expression");
+	  else
+		  System.out.println("Invalid expression");
+	  scan.close();
+     }
+	
+	
+	  public static boolean isValid(String expr)
+	  {
+	  	StackA st = new StackA();
 
-			switch(choice)
-			{
-			 case 1:
-				List.displayList();
-				break;
-			 case 2:
-				System.out.print("Enter the element to be inserted : ");
-				data = scan.nextInt();
-				List.insertAtEnd(data);
-				break;
-			 case 3:
-				System.out.print("Enter the element to be inserted : ");
-				data = scan.nextInt();
-				System.out.print("Enter the element before which to insert : ");
-				x = scan.nextInt();
-				List.insertBefore(data,x);
-				break;
-			 case 4:
-				System.out.print("Enter the element to be inserted : ");
-				data = scan.nextInt();
-				System.out.print("Enter the position at which to insert : ");
-				k = scan.nextInt();
-				List.insertAtPosition(data,k);
-				break;
-			 case 5:
-				System.out.print("Enter the element to be deleted : ");
-				data = scan.nextInt();
-				List.deleteNode(data);	
-				break;
-			 case 6:
-				List.reverse();
-				break;
-			 default:
-				System.out.println("Wrong choice");
-			}
-		}/*End of while */
+	  	char ch;
+	  	for(int i=0; i<expr.length(); i++)
+	  	{
+	  		if(expr.charAt(i)=='(' || expr.charAt(i)=='{' || expr.charAt(i)=='[')
+	  			st.push(expr.charAt(i));
 
-	}
+	  		if(expr.charAt(i)==')' || expr.charAt(i)=='}' || expr.charAt(i)==']')
+	  			if(st.isEmpty())    
+	  			{
+	  				System.out.println("Right parentheses are more than left parentheses");
+	  				return false;
+	  			}
+	  			else
+	  			{
+	  				ch=st.pop();
+	  				if(!matchParentheses(ch,expr.charAt(i))) 
+	  				{
+	  					System.out.println("Mismatched parentheses are : "); 
+	  					System.out.println(ch + " and " + expr.charAt(i));	
+	  					return false;
+	  				}
+	  			}
+	  	}
 
+	  	if(st.isEmpty()) 
+	  	{
+	  		System.out.println("Balanced Parentheses"); 
+	  		return true;
+	  	}
+	  	else 
+	  	{
+	  		System.out.println("Left parentheses are more than right parentheses");	
+	  		return false;
+	  	}
+	  	
+	  }
+
+	  public static boolean matchParentheses(char leftPar,char rightPar)
+	  {
+	  	if(leftPar=='[' && rightPar==']')
+	  		return true;
+	  	if(leftPar=='{' && rightPar=='}')
+	  		return true;	
+	  	if(leftPar=='(' && rightPar==')')
+	  		return true;
+	  	return false;
+	  }
 }
-
-
