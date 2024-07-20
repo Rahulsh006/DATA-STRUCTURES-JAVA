bd61b040e33dbf0e5427acf9cf7dcc27bacd94dc
RAHUL S H <85486874+Rahulsh006@users.noreply.github.com>
2024-07-20 14:47:30 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -3,50 +3,72 @@ Copyright (C) Deepali Srivastava - All Rights Reserved
 This code is part of DSA course available on CourseGalaxy.com    
 */
 
-package sortedLinkedList;
+package OpenAddressing;
 
 import java.util.Scanner;
 
 public class Demo 
 {
-	public static void main(String[] args)
-	{ 
-		int choice,data;
-	
-	    Scanner scan = new Scanner(System.in);	
-	
-  	    SortedLinkedList List = new SortedLinkedList();
-  	    List.createList();
-   
-	    while(true)
-	    {
-		  System.out.println("1.Display List");
-		  System.out.println("2.Insert a new node");
-		  System.out.println("3.Search");
-		  System.out.println("4.Exit");
-		  System.out.print("Enter your choice : ");
-		  choice = scan.nextInt();
-		  if(choice==4)
-			 break;
+	public static void main(String[] args) 
+	{
+		int id,choice;
+		String name;
 		
-		  switch(choice)
-		  {
-		    case 1:
-			  List.displayList();
-			  break;
-		    case 2:
-			  System.out.println("Enter the element to be inserted : ");
-			  data = scan.nextInt();
-			  List.insertInOrder(data);
-			  break;
-		    case 3:
-			   System.out.println("Enter the element to be searched : ");
-			   data=scan.nextInt();
-			   List.search(data);
-			   break;
-		  default:
-			   System.out.println("Wrong choice\n");
+		Scanner scan = new Scanner(System.in);
+			
+		System.out.print("Enter initial size of table : ");
+		int size = scan.nextInt();
+			
+		HashTable table = new HashTable(size);
+		
+		while(true)
+		{
+			System.out.println("1.Insert a record");
+			System.out.println("2.Search a record");
+			System.out.println("3.Delete a record");
+			System.out.println("4.Display table");
+			System.out.println("5.Exit");
+			
+			System.out.print("Enter your choice : ");
+	        choice = scan.nextInt();
+
+	        if (choice == 5)
+	                break;
+			
+	        switch(choice)
+			{
+				case 1 : 
+				   System.out.print("Enter student id : ");
+				   id = scan.nextInt();
+				   System.out.print("Enter student name : ");
+				   name = scan.next();
+				
+				   studentRecord aRecord = new studentRecord(id,name);
+				   
+				   table.Insert(aRecord);
+				   break;
+				case 2 : 
+				   System.out.print("Enter a key to be searched : ");
+				   id = scan.nextInt();
+				   aRecord = table.Search(id); 
+				  				   
+				   if( aRecord==null )
+					   System.out.println("Key not found");
+				   else
+					   System.out.println(aRecord);
+					   
+				   break;
+				case 3: 
+				   System.out.print("Enter a key to be deleted : ");
+				   id = scan.nextInt();
+				   table.Delete(id);
+				   break;
+				case 4:
+				   table.DisplayTable();
+				   break;
+				
+			}
 		}
-	}/*End of while*/
-  }/*End of main()*/
+		scan.close();
+	}
 }
