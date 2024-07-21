a7e96b42278d70b2ccd71ef760eb9cee5757425c
RAHUL S H <85486874+Rahulsh006@users.noreply.github.com>
2024-07-21 15:32:14 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -3,50 +3,80 @@ Copyright (C) Deepali Srivastava - All Rights Reserved
 This code is part of DSA course available on CourseGalaxy.com    
 */
 
-package heap;
-
+package binarySearchTree;
 import java.util.Scanner;
-
-
-public class Demo
+public class Demo 
 {
 	public static void main(String[] args) 
 	{
+		BinarySearchTree bt = new BinarySearchTree(); 
+		int choice,x;
+		
 		Scanner scan = new Scanner(System.in);	
-					
-		Heap h = new Heap(20);
-				
-		int choice,value;
-				
+		
 		while(true)
 		{
-			System.out.println("1.Insert");
-			System.out.println("2.Delete root");
-			System.out.println("3.Display");
-			System.out.println("4.Exit");
-			System.out.print("Enter your choice : ");
+			System.out.println("1.Display Tree");
+			System.out.println("2.Search");
+			System.out.println("3.Insert a new node");
+			System.out.println("4.Delete a node");
+			System.out.println("5.Preorder Traversal");
+			System.out.println("6.Inorder Traversal");
+			System.out.println("7.Postorder Traversal");
+			System.out.println("8.Height of tree");
+			System.out.println("9.Find Minimum key");
+			System.out.println("10.Find Maximum key");
+			System.out.println("11.Quit");
+			System.out.println("Enter your choice : ");
 			choice = scan.nextInt();
-			
-			if(choice==4)
+
+			if(choice==11)
 				break;
-			
+
 			switch(choice)
 			{
-			 case 1:
-				System.out.print("Enter the value to be inserted : ");
-				value = scan.nextInt();
-				h.insert(value);
+			case 1:
+				bt.display();
 				break;
-			 case 2:
-				System.out.println("Maximum value is " + h.deleteRoot());
+			case 2: 
+				System.out.print("Enter the key to be searched : ");
+				x = scan.nextInt();
+				
+				if(bt.search(x))
+					System.out.println("Key found");
+				else
+					System.out.println("Key not found");
 				break;
-			 case 3:
-				h.display();
+			case 3:
+				System.out.println("Enter the key to be inserted : ");
+				x = scan.nextInt();
+				bt.insert(x);
 				break;
-	 		 default:
-				System.out.println("Wrong choice");
-			}
-		}
+			case 4:
+				System.out.println("Enter the key to be deleted : ");
+				x = scan.nextInt();
+				bt.delete(x);
+				break;
+			 case 5:
+				bt.preorder();
+				break;
+			 case 6:
+				bt.inorder();
+				break;
+			 case 7:
+				bt.postorder();
+				break;
+			 case 8:
+				System.out.println("Height of tree is " + bt.height());
+				break;
+			 case 9:
+				System.out.println("Minimum key is " + bt.min());
+				break;
+			 case 10:
+				System.out.println("Maximum key is " + bt.max());
+				break;
+			}/*End of switch */
+		}/*End of while */
 		scan.close();
 	}
 }
--- Demo1.java
+++ Demo1.java
@@ -3,90 +3,83 @@ Copyright (C) Deepali Srivastava - All Rights Reserved
 This code is part of DSA course available on CourseGalaxy.com    
 */
 
-package heap;
+package binarySearchTree;
+
+import java.util.Scanner;
 
 public class Demo1 
 {
-	public static void buildHeap_TopDown(int[] a, int n)
-    {
-    	for(int i=2; i<=n; i++)
-    		restoreUp(i,a);
-    }
-	
-    public static void buildHeap_BottomUp(int[] a, int n)
-    {
-    	for(int i=n/2; i>=1; i--)
-    		restoreDown(i,a,n);
-    }
-	
-    private static void restoreUp(int i, int[] a)
-    {
-    	int k=a[i];  
-    	int iparent=i/2;
-    	
-    	while(a[iparent]<k)      /* No sentinel : while(iparent>=1 && a[iparent]<k)  */
-    	{
-    		a[i]=a[iparent];
-    		i=iparent;
-    		iparent=i/2;
-    	}
-    	a[i]=k; 
-    }
-    
-    private static void restoreDown(int i, int[] a, int n)
-    {
-    	int k=a[i];
-    	int lchild=2*i, rchild=lchild+1;
-    		
-    	while(rchild<=n)
-    	{
-    		if( k>=a[lchild] && k>=a[rchild] )  
-    		{
-    			a[i]=k;
-    			return;
-    		}
-    		else if(a[lchild] > a[rchild])
-    		{
-    			a[i]=a[lchild]; 
-    			i=lchild;
-    		}
-    		else
-    		{
-    			a[i]=a[rchild];
-    			i=rchild;
-    		}
-    		lchild=2*i;
-    		rchild=lchild+1;
-    	}
-
-    	/*If number of nodes is even*/	
-    	if(lchild==n && k<a[lchild]) 
-    	{
-    		a[i]=a[lchild];
-    		i=lchild;
-    	}
-    	a[i]=k;
-    }
-
 	public static void main(String[] args) 
 	{
-		int a1[] = {99999,1,4,5,7,9,10};
-		int n1=a1.length-1;
+		BinarySearchTree bt = new BinarySearchTree(); 
+		int choice,x;
 		
-		buildHeap_BottomUp(a1,n1);
+		Scanner scan = new Scanner(System.in);	
 		
-		for(int i=1; i<=n1; i++ )
-			System.out.print(a1[i] + " ");
-		System.out.println();
-		
-		
-		int a2[] = {99999,1,4,5,7,9,10};
-		int n2=a2.length-1;
-		
-		buildHeap_TopDown(a2,n2);
-		
-		for(int i=1; i<=n2; i++ )
-			System.out.print(a2[i] + " ");
-		System.out.println();
+		while(true)
+		{
+			System.out.println("1.Display Tree");
+			System.out.println("2.Search");
+			System.out.println("3.Insert a new node");
+			System.out.println("4.Delete a node");
+			System.out.println("5.Preorder Traversal");
+			System.out.println("6.Inorder Traversal");
+			System.out.println("7.Postorder Traversal");
+			System.out.println("8.Height of tree");
+			System.out.println("9.Find Minimum key");
+			System.out.println("10.Find Maximum key");
+			System.out.println("11.Quit");
+			System.out.println("Enter your choice : ");
+			choice = scan.nextInt();
+
+			if(choice==11)
+				break;
+
+			switch(choice)
+			{
+			case 1:
+				bt.display();
+				break;
+			case 2: 
+				System.out.print("Enter the key to be searched : ");
+				x = scan.nextInt();
+				
+				if(bt.search1(x))
+					System.out.println("Key found");
+				else
+					System.out.println("Key not found");
+				break;
+			case 3:
+				System.out.println("Enter the key to be inserted : ");
+				x = scan.nextInt();
+				bt.insert1(x);
+				break;
+			case 4:
+				System.out.println("Enter the key to be deleted : ");
+				x = scan.nextInt();
+				bt.delete1(x);
+				break;
+			 case 5:
+				bt.preorder();
+				break;
+			 case 6:
+				bt.inorder();
+				break;
+			 case 7:
+				bt.postorder();
+				break;
+			 case 8:
+				System.out.println("Height of tree is " + bt.height());
+				break;
+			 case 9:
+				System.out.println("Minimum key is " + bt.min1());
+				break;
+			 case 10:
+				System.out.println("Maximum key is " + bt.max1());
+				break;
+			}/*End of switch */
+		}/*End of while */
+		scan.close();
+	
 	}
 }
